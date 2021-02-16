b2884d6ae7bc92ad703832a764696636fb3ed3b9
KianAshrafganjouei <kashr085@uottawa.ca>
2021-02-16 17:10:03 -0500

    worked on card component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.componenet";
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((resp) => resp.json())
+      .then((monsterFeatures) => this.setState({ monsters: monsterFeatures }));
+  }
+
   render() {
-    return <div> </div>;
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <input type="search" placeholder="search for monster"></input>
+        <CardList monsters={this.state.monsters} />
+      </div>
+    );
   }
 }
-
-export default App;
---
+++ src/components/cards/card.component.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import "./card.styles.css";
+export const Card = (props) => {
+  return <div className="card-container">{props.monster.name}</div>;
+};
