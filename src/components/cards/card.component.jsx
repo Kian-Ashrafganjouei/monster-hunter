d7a4592f0ee4c8bf92790fe5c6e687897336724e
KianAshrafganjouei <kashr085@uottawa.ca>
2021-02-16 18:08:40 -0500

    added monster photos and further broke the card componenent

--- src/components/cards/card.component.jsx
+++ src/components/cards/card.component.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 import "./card.styles.css";
 export const Card = (props) => {
-  return <div className="card-container">{props.monster.name}</div>;
+  return (
+    <div className="card-container">
+      <img
+        alt="monster"
+        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+      />
+      <h2>{props.monster.name}</h2>
+    </div>
+  );
 };
