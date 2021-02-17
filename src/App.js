aec08164fde0f9c004bed408a16061f7bb745b64
KianAshrafganjouei <kashr085@uottawa.ca>
2021-02-17 10:34:50 -0500

    finished search feature added styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { CardList } from "./components/card-list/card-list.componenet";
-
+import { SearchBox } from "./components/search-box/search-box.component";
+import "./App.css";
 export default class App extends Component {
   constructor() {
     super();
@@ -16,12 +17,24 @@ export default class App extends Component {
       .then((monsterFeatures) => this.setState({ monsters: monsterFeatures }));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
         <h1>Monsters Rolodex</h1>
-        <input type="search" placeholder="search for monster"></input>
-        <CardList monsters={this.state.monsters} />
+        <SearchBox
+          placeholder="begin the hunt"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
